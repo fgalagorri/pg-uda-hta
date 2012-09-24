bbf8c028c186cd2754dcc0744bef7a9773a36304
fgalagorri@gmail.com <fgalagorri@gmail.com@dcb8883c-4fa3-2fed-8c71-46e3153f6227>
2012-09-24 02:03:53 +0000

    Agrego interfaz para los accesos a las BD de los dispositivos

--- trunk/UDA-HTA/HMSDataAccess/Class1.cs
+++ trunk/UDA-HTA/HMSDataAccess/Class1.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Interfaces;
 
 namespace HMSDataAccess
 {
-    public class Class1
+    public class Class1 : IDeviceDataAccess
     {
     }
 }
---
+++ trunk/UDA-HTA/Interfaces/IDeviceDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interfaces
+{
+    public interface IDeviceDataAccess
+    {
+
+        #region list members
+
+        ICollection<Patient> ListPatients()
+        {
+
+        }
+
+        ICollection<Report> ListReports()
+        {
+
+        }
+
+        #endregion
+
+
+
+
+    }
+}
