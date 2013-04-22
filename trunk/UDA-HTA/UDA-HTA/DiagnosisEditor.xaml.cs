ae631c9e48d37abdef401d3ef82f029352409a5b
fgalagorri@gmail.com <fgalagorri@gmail.com@dcb8883c-4fa3-2fed-8c71-46e3153f6227>
2013-04-22 00:35:02 +0000

    Diagnósticos y correcciones de reunión.

---
+++ trunk/UDA-HTA/UDA-HTA/DiagnosisEditor.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UDA_HTA
+{
+    /// <summary>
+    /// Interaction logic for DiagnosisEditor.xaml
+    /// </summary>
+    public partial class DiagnosisEditor : Window
+    {
+        public DiagnosisEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccept_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- trunk/UDA-HTA/UDA-HTA/UserControls/ReportCreation/PatientInformation.xaml.cs
+++ trunk/UDA-HTA/UDA-HTA/UserControls/ReportCreation/PatientInformation.xaml.cs
@@ -1,15 +1,35 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
 
 namespace UDA_HTA.UserControls.ReportCreation
 {
+
     /// <summary>
     /// Interaction logic for PatientInformation.xaml
     /// </summary>
     public partial class PatientInformation : UserControl
     {
+        double _imc;
+
         public PatientInformation()
         {
             InitializeComponent();
+        }
+
+        private void CalculateImc(object sender, TextChangedEventArgs e)
+        {
+            double height, weight;
+            txtHeight.Text.Replace(".", ",");
+
+            if (double.TryParse(txtHeight.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out height)
+                && double.TryParse(txtWeight.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out weight))
+            {
+                _imc = weight/(height*height);
+                lblImc.Text = _imc.ToString("0.##", CultureInfo.InvariantCulture);
+            }
+            else
+                lblImc.Text = "";
         }
     }
 }
