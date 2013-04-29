931f5fa41bf4b676b499571538fc924619690c35
fgalagorri@gmail.com <fgalagorri@gmail.com@dcb8883c-4fa3-2fed-8c71-46e3153f6227>
2013-04-29 12:23:26 +0000

    Agrego selector de Medicación

---
+++ trunk/UDA-HTA/UDA-HTA/UserControls/MedicationSelector.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UDA_HTA.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MedicationSelector.xaml
+    /// </summary>
+    public partial class MedicationSelector : Window
+    {
+        public MedicationSelector()
+        {
+            InitializeComponent();
+
+            cmbCategory.Items.Add("Betabloqueante");
+            cmbCategory.Items.Add("Diuréticos");
+            cmbCategory.Items.Add("Calcioantagonistas"); 
+            cmbCategory.Items.Add("IECA");
+            cmbCategory.Items.Add("ARA2");
+            cmbCategory.Items.Add("Acción Central");
+            
+            cmbActive
+    }
+}
