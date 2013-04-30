138b93e83735b71668bab985cd1974ca28152774
fgalagorri@gmail.com <fgalagorri@gmail.com@dcb8883c-4fa3-2fed-8c71-46e3153f6227>
2013-04-30 01:51:55 +0000

    Agrego el MedicationSelector completo

--- UDA-HTA/UDA-HTA/UserControls/MedicationSelector.xaml.cs
+++ UDA-HTA/UDA-HTA/UserControls/MedicationSelector.xaml.cs
@@ -18,17 +18,86 @@ namespace UDA_HTA.UserControls
     /// </summary>
     public partial class MedicationSelector : Window
     {
+        public string category { get; set; }
+        public string active { get; set; }
+        public string name { get; set; }
+
         public MedicationSelector()
         {
             InitializeComponent();
 
-            cmbCategory.Items.Add("Betabloqueante");
-            cmbCategory.Items.Add("Diuréticos");
-            cmbCategory.Items.Add("Calcioantagonistas"); 
-            cmbCategory.Items.Add("IECA");
-            cmbCategory.Items.Add("ARA2");
             cmbCategory.Items.Add("Acción Central");
-            
-            cmbActive
+            cmbCategory.Items.Add("ARA2");
+            cmbCategory.Items.Add("Betabloqueante");
+            cmbCategory.Items.Add("Calcioantagonistas");
+            cmbCategory.Items.Add("Diuréticos");
+            cmbCategory.Items.Add("IECA");
+        }
+
+        private void cmbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbCategory.SelectedIndex >= 0)
+            {
+                cmbActive.Items.Clear();
+                cmbActive.Items.Add(cmbCategory.SelectedValue + " item 1");
+                cmbActive.Items.Add(cmbCategory.SelectedValue + " item 2");
+                cmbActive.Items.Add(cmbCategory.SelectedValue + " item 3");
+                cmbActive.Items.Add(cmbCategory.SelectedValue + " item 4");
+                cmbActive.SelectedIndex = -1;
+                cmbActive.IsEnabled = true;
+            }
+            else
+            {
+                cmbActive.SelectedIndex = -1;
+                cmbActive.Items.Clear();
+                cmbActive.IsEnabled = false;
+            }
+
+            cmbName.IsEnabled = false;
+            cmbName.Items.Clear();
+            cmbName.SelectedIndex = -1;
+        }
+
+        private void cmbActive_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbActive.SelectedIndex >= 0)
+            {
+                cmbName.Items.Clear();
+                cmbName.Items.Add(cmbActive.SelectedValue + " item 1");
+                cmbName.Items.Add(cmbActive.SelectedValue + " item 2");
+                cmbName.Items.Add(cmbActive.SelectedValue + " item 3");
+                cmbName.Items.Add(cmbActive.SelectedValue + " item 4");
+                cmbName.SelectedIndex = -1;
+                cmbName.IsEnabled = true;
+            }
+            else
+            {
+                cmbName.SelectedIndex = -1;
+                cmbName.Items.Clear();
+                cmbName.IsEnabled = false;
+            }
+        }
+
+        private void cmbName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbName.SelectedIndex >= 0)
+                btnAccept.IsEnabled = true;
+            else
+                btnAccept.IsEnabled = false;
+        }
+
+        private void btnAccept_Click(object sender, RoutedEventArgs e)
+        {
+            category = cmbCategory.SelectedValue.ToString();
+            active = cmbActive.SelectedValue.ToString();
+            name = cmbName.SelectedValue.ToString();
+
+            Close();
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
