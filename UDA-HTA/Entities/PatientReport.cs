e8aca00ad90f5dbadf9ad5a4434c6580cfb5c7d9
fgalagorri@gmail.com <fgalagorri@gmail.com@dcb8883c-4fa3-2fed-8c71-46e3153f6227>
2013-09-02 01:01:42 +0000

    Desplegar nombre del dispositivo al importar

--- UDA-HTA/Entities/PatientReport.cs
+++ UDA-HTA/Entities/PatientReport.cs
@@ -5,17 +5,12 @@ namespace Entities
     public class PatientReport
     {
         public string PatientName { get; set; }
-
         public string PatientLastName { get; set; }
-
         public string PatientDocument { get; set; }
-
         public string PatientId { get; set; }
-
         public DateTime? ReportDate { get; set; }
-
         public int ReportDevice { get; set; }
-
+        public string ReportDeviceName { get; set; }
         public string ReportId { get; set; }
     }
 }
